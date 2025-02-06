c0f8b58ffd630589554c94a7e85696762b3518d6
Ben Ng <benng512@gmail.com>
2025-02-06 14:57:01 +0800

    20241210

---
+++ demo-sb-helloworld/src/test/java/com/bootcamp/demosb/demo_sb_helloworld/DemoSbHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.demosb.demo_sb_helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSbHelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
