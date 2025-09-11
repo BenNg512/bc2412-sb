886205a76e1ccde1d9d309d009d3a3914934a69e
Ben Ng <benng512@gmail.com>
2025-09-11 16:02:02 +0800

    updated cryto currencies files

---
+++ demo-sb-coin-web/src/README.md
@@ -0,0 +1,2 @@
+![Dashboard](image.png)
+This project is to get the API data of crypto currencies from Coingecko, with auto refresh function so the latest prices of crypto currencies will be auto updated on this page.
---
+++ demo-sb-coin-web/src/main/java/com/bootcamp/web/DemoBcWebpageApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBcWebpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBcWebpageApplication.class, args);
+	}
+
+}
